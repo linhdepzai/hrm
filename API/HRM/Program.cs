3329013302b5fe8f0566dcc131ae8d46ecfa2322
Đặng Linh <104380101+linhdepzai@users.noreply.github.com>
2023-07-12 19:35:19 +0700

    fix backend

--- API/HRM/Program.cs
+++ API/HRM/Program.cs
@@ -77,7 +77,8 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-} else
+}
+else
 {
     app.UseSwagger();
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
